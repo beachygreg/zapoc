422d5205fc41ece955c183d84cdfaaeeeb086acc
Paul Sandulescu <paul_sandulescu@silanis.com>
2014-04-17 12:25:02 -0400

    added chat server

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var express = require('express');
 var socketio = require('socket.io');
+var uuid = require('uuid');
+var _ = require('lodash');
 
 
 var app = express();
@@ -9,7 +11,81 @@ var ws = socketio.listen(server);
 
 app.use(express.static(__dirname + '/public'));
 
+var clients = {};
+var sockets = {};
 
+function broadcastChat(clients, message, data) {
+  _.each(clients, function(client, playerUuid) {
+    if (client.socket) {
+      client.socket.emit(message, data);
+    }
+  });
+}
 
+function getActivePlayers() {
+  return _(clients).filter('socket').pluck('nickName').value();
+}
+
+ws.sockets.on('connection', function(socket) {
+  socket.on('startChat', function(data) {
+    var client;
+
+    if (data.playerUuid) {
+      console.log("connected UUID: ", data.playerUuid);
+      client = clients[data.playerUuid];
+      if (client) client.socket = socket;
+    }
+
+    if (!client) {
+      client = {
+        playerUuid: uuid.v4(),
+        nickName: data.nickName,
+        socket: socket
+      };
+      clients[client.playerUuid] = client;
+    }
+
+    sockets[socket.id] = client;
+    socket.emit('chatConnected', {
+      nickName: client.nickName, playerUuid: client.playerUuid,
+      players: getActivePlayers()
+    });
+    broadcastChat(clients, 'joinedChat', {
+      nickName: client.nickName,
+      players: getActivePlayers()
+    });
+
+    socket.on('chat', function(data) {
+      broadcastChat(clients, 'chat', {
+        nickName: sockets[socket.id].nickName,
+        message: data.message
+      });
+    });
+
+    socket.on('endChat', function(data) {
+      var client;
+      if (sockets[socket.id]) {
+        client = sockets[socket.id];
+        delete clients[client.playerUuid];
+        delete sockets[socket.id];
+        broadcastChat(clients, 'leftChat', {
+          nickName: client.nickName,
+          players: getActivePlayers()
+        });
+      }
+    });
+  });
+
+  socket.on('disconnect', function() {
+    var client = sockets[socket.id];
+    if (client) {
+      delete client.socket;
+      broadcastChat(clients, 'disconnectedChat', {
+        nickName: client.nickName,
+        players: getActivePlayers()
+      });
+    }
+  });
+});
 
 server.listen(4000);
---
+++ public/app/scripts/chat-controller.js
@@ -0,0 +1,69 @@
+zapocApp.controller('ChatController', [
+  '$scope',
+  'socketService',
+  'localStorageService',
+  function($scope, socketService, $localStorage) {
+    $scope.messages = [];
+    $scope.players = [];
+    $scope.chatConnected = false;
+    $scope.nickName = $localStorage.get('nickName');
+
+    if ($localStorage.get('playerUuid')) {
+      $scope.playerUuid = $localStorage.get('playerUuid');
+      socketService.emit('startChat', { nickName: $scope.nickName, playerUuid: $scope.playerUuid });
+    }
+
+    socketService.on('chatConnected', function(data) {
+      $scope.chatConnected = true;
+      $scope.playerUuid = data.playerUuid;
+      $scope.players = data.players;
+      $scope.saveLocally();
+    });
+
+    $scope.saveLocally = function() {
+      $localStorage.set('playerUuid', $scope.playerUuid);
+      $localStorage.set('nickName', $scope.nickName);
+    };
+
+    socketService.on('joinedChat', function(data) {
+      $scope.messages.push({system: true, nickName: data.nickName, message: 'joined!'});
+      if (data.players) $scope.players = data.players;
+    });
+
+    socketService.on('leftChat', function(data) {
+      $scope.messages.push({system: true, nickName: data.nickName, message: 'left!'});
+      if (data.players) $scope.players = data.players;
+    });
+
+    socketService.on('chat', function(data) {
+      $scope.messages.push(data);
+    });
+
+    socketService.on('disconnectedChat', function(data) {
+      $scope.messages.push({system: true, nickName: data.nickName, message: 'got disconnected!'});
+      if (data.players) $scope.players = data.players;
+    });
+
+    $scope.connectChat = function() {
+      if (!$scope.nickName) return;
+      socketService.emit('startChat', { nickName: $scope.nickName, playerUuid: $scope.playerUuid });
+    };
+
+    $scope.sendChatMessage = function() {
+      socketService.emit('chat', {message: $scope.chatText});
+      $scope.chatText = '';
+    };
+
+    $scope.onEnter = function(event, callback) {
+      if (event.keyCode === 13) callback();
+    };
+
+    $scope.disconnectChat = function() {
+      socketService.emit('endChat', {});
+      $scope.playerUuid = null;
+      $scope.saveLocally();
+      $scope.chatConnected = false;
+      $scope.players = [];
+    };
+  }
+]);
