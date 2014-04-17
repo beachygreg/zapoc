a083f73c59ceb93d7b7aeb11ed47126199ae1507
gregory beauchamp <beachy.greg@gmail.com>
2014-04-17 13:23:39 -0400

    moving things

---
+++ server/specialZombies.js
@@ -0,0 +1,59 @@
+var specialZombieDefinitions = [
+  {
+    name : "Boomer",
+    speed : 4,
+    power : 2,
+    toughness : 4,
+    special : "When killed, all survivors in the room receive an infection token."
+  },
+  {
+    name : "Roper",
+    speed : 3,
+    power : 2,
+    toughness : 5,
+    special : "Instead of attacking, may attempt to grab one survivor in line of sight.  Roll D: 1-2: miss; 3-4:Move survivor to adjacent square; 5-6: Move survivor and knock prone."
+  },
+  {
+    name : "Hunter",
+    speed : 3,
+    power : 2,
+    toughness : 4,
+    special : "Instead of moving, may leap up to 5 squares in a straight line.  All survivors adjacent to the landing square are knocked prone."
+  },
+  {
+    name : "Brute",
+    speed : 3,
+    power : 3,
+    toughness : 6,
+    special : "On attack, knock target back one square."
+  },
+  {
+    name : "Smoker",
+    speed : 3,
+    power : 2,
+    toughness : 5,
+    special : "Any room containing the smoker is considered to be dark."
+  },
+  {
+    name : "Reaper",
+    speed : 4,
+    power : 2,
+    toughness : 4,
+    special : "Any survivor injured by an attack begins to bleed."
+  },
+  {
+    name : "Torch",
+    speed : 3,
+    power : 1,
+    toughness : 4,
+    special : "On death, containing room is lit on fire.  Any adjacent survivor that activates rolls D: 1-3 lose one health; 4-6 no effect."
+  },
+  {
+    name : "Banshee",
+    speed : 3,
+    power : 1,
+    toughness : 4,
+    special : "Any move or attack performed by the banshee is considered to be a noise trigger."
+  }
+    
+];
