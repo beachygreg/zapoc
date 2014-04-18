af6121a3f249db1807a7c9940af24e02058bd8a6
Dave Lawson <davelawson@hotmail.com>
2014-04-18 17:38:36 -0400

    Expanded survivor model and zombie keeper model a tad.  More tinkering with the survivor sidebar.

--- public/app/scripts/survivor-sidebar-controller.js
+++ public/app/scripts/survivor-sidebar-controller.js
@@ -2,40 +2,62 @@ zapocApp.controller('SurvivorSidebarController', ['$scope', function ($scope) {
 
     $scope.players = [
         {
-            "name": "Player #1",
+            "name": "Dave",
             "role": "SURVIVOR",
             "survivor": {
                 "energy": 1,
-                "health": 1
+                "health": 1,
+                "infection": [true,false],
+                "background": {
+                    "energy": 5,
+                    "health": 5
+                }
             }
         },
         {
-            "name": "Player #2",
+            "name": "Q-Dawg",
             "role": "SURVIVOR",
             "survivor": {
                 "energy": 2,
-                "health": 2
+                "health": 2,
+                "infection": [true,false,false],
+                "background": {
+                    "energy": 5,
+                    "health": 5
+                }
             }
         },
         {
-            "name": "Player #3",
+            "name": "Sandy",
             "role": "SURVIVOR",
             "survivor": {
                 "energy": 3,
-                "health": 3
+                "health": 3,
+                "infection": [true,false,false,false],
+                "background": {
+                    "energy": 5,
+                    "health": 5
+                }
             }
         },
         {
-            "name": "Player #4",
+            "name": "G-man",
             "role": "SURVIVOR",
             "survivor": {
                 "energy": 4,
-                "health": 4
+                "health": 4,
+                "infection": [true,false,false,false,false],
+                "background": {
+                    "energy": 5,
+                    "health": 5
+                }
             }
         },
         {
-            "name": "Player #5",
-            "role": "ZOMBIE_MASTER"
+            "name": "Mr. Poitras",
+            "role": "ZOMBIE_MASTER",
+            "horror": 3,
+            "horrorCards": []
         }
     ];
 }]);
